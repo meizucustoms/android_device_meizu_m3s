0ddb101e3d2f213b407278a6dd51b6bd8cab7598
ElXreno <elxreno@gmail.com>
2019-06-03 17:26:01 +0300

    Disable SELinux

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -37,7 +37,7 @@ BOARD_HARDWARE_CLASS += \
     hardware/cyanogen/cmhw
 
 # Kernel
-BOARD_KERNEL_CMDLINE := bootopt=64S3,32N2,64N2 elevator=deadline
+BOARD_KERNEL_CMDLINE := bootopt=64S3,32N2,64N2 androidboot.selinux=permissive
 BOARD_KERNEL_BASE := 0x40078000
 BOARD_KERNEL_PAGESIZE := 2048
 BOARD_MKBOOTIMG_ARGS := --board 1466056865 --kernel_offset 0x00008000 --ramdisk_offset 0x04f88000 --tags_offset 0x03f88000
